b5940dcf6674b97adbff63b4e23a55f3f5fbc62c
github username <github email address>
2018-07-25 23:14:43 -0700

    remove edit user profile function, change look and feel of login and account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,19 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :show, :edit] do
+  resources :users do
     resources :user_cards, only: [:new, :index]
   end
   #resources :deck_cards, only: [:new, :index]
