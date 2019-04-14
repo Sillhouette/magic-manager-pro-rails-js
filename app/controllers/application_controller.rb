3ae268f6e58f4899f457b8403957242cfda698ad
github username <raustin.melchior@gmail.com>
2019-04-14 16:47:56 -0700

    prevent current_user from being looked for if already exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    @user = (User.find_by(id: session[:user_id]) || User.new)
+    @user ||= User.find_by(id: session[:user_id]) || User.new
   end
 
   def logged_in?
