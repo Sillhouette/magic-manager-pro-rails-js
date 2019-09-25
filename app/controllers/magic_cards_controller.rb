5ebd3a46f7dd23c4543376f2b5ccb9413e68a7d8
github username <raustin.melchior@gmail.com>
2019-09-25 00:17:02 -0700

    try to decrease mem usage on index of magic cards

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -8,10 +8,8 @@ class MagicCardsController < ApplicationController
   end
 
   def index
-    @cards = MagicCard.where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)"))
-    @cards2 = MagicCard.where(multiverse_ids: [])
-    @cards3 = @cards + @cards2
-    @pagy, @magic_cards = pagy_array(@cards3, items: 30)
+    @cards = [MagicCard.where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")), MagicCard.where(multiverse_ids: [])]
+    @pagy, @magic_cards = pagy_array(@cards, items: 10)
     respond_to do |format|
       format.html { render :index }
       format.json { render json: MagicCard.all, include: '*.*'}
