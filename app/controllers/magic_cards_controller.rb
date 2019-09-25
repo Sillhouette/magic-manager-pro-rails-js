546a6de4b74447f0ec19a7daa2860d715e954f36
github username <raustin.melchior@gmail.com>
2019-09-25 00:31:09 -0700

    attempt to fix memory issue

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -8,7 +8,7 @@ class MagicCardsController < ApplicationController
   end
 
   def index
-    @cards = [MagicCard.where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")), MagicCard.where(multiverse_ids: [])]
+    @cards = MagicCard.where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")) + MagicCard.where(multiverse_ids: [])
     @pagy, @magic_cards = pagy_array(@cards, items: 10)
     respond_to do |format|
       format.html { render :index }
