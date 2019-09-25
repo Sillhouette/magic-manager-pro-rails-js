46e114057b89ea17541d96ebfde6351af12ae171
github username <raustin.melchior@gmail.com>
2019-09-25 11:09:06 -0700

    try to lighten the load when loading cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_001949) do
+ActiveRecord::Schema.define(version: 2019_09_25_073951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -133,6 +133,9 @@ ActiveRecord::Schema.define(version: 2019_09_10_001949) do
     t.string "watermark"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["multiverse_ids"], name: "index_magic_cards_on_multiverse_ids"
+    t.index ["name"], name: "index_magic_cards_on_name"
+    t.index ["scryfall_id"], name: "index_magic_cards_on_scryfall_id"
   end
 
   create_table "related_cards", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
