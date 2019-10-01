0bef631ed41421a3441bee40ac07ac02bf1efd3e
github username <raustin.melchior@gmail.com>
2019-10-01 09:11:08 -0700

    something broke

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -12,6 +12,7 @@ class MagicCardsController < ApplicationController
     respond_to do |format|
       format.html {
         name = params["magic_card_name"].split(" - ")[0] if params["magic_card_name"]
+        puts name
         if name
           @pagy, @magic_cards = pagy(MagicCard.where('lower(name) LIKE ?', "%#{name}%").order(:name), items: 10)
         else
