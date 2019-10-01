56ed1f584e5accc8273a76c4f02b3d835c52ba1f
github username <raustin.melchior@gmail.com>
2019-09-30 22:15:05 -0700

    add sorting of cards

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -8,30 +8,33 @@ class MagicCardsController < ApplicationController
   end
 
   def index
-    puts params
-    @cards = MagicCard.select(
-      :id,
-      :image_uris,
-      :name,
-      :set,
-      :set_name,
-      :mana_cost,
-      :type_line,
-      :oracle_text,
-      :power,
-      :toughness
-    ).where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")) + MagicCard.select(
-      :id,
-      :image_uris,
-      :name,
-      :set,
-      :set_name,
-      :mana_cost,
-      :type_line,
-      :oracle_text,
-      :power,
-      :toughness
-    ).where(multiverse_ids: [])
+    if name = params["magic_card_name"]
+      @cards = MagicCard.where('name LIKE ?', "%#{name.split(" - ")[0]}%")
+    else
+      @cards = MagicCard.select(
+        :id,
+        :image_uris,
+        :name,
+        :set,
+        :set_name,
+        :mana_cost,
+        :type_line,
+        :oracle_text,
+        :power,
+        :toughness
+      ).where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")) + MagicCard.select(
+        :id,
+        :image_uris,
+        :name,
+        :set,
+        :set_name,
+        :mana_cost,
+        :type_line,
+        :oracle_text,
+        :power,
+        :toughness
+      ).where(multiverse_ids: [])
+    end
     @pagy, @magic_cards = pagy_array(@cards, items: 10)
     respond_to do |format|
       format.html { render :index }
