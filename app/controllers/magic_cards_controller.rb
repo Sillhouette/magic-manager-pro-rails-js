b8dd3f2d93c90822dd4fd8d2920a9b4d3304913d
github username <raustin.melchior@gmail.com>
2019-10-01 09:16:32 -0700

    missed closing parenthesis

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -13,7 +13,7 @@ class MagicCardsController < ApplicationController
       format.html {
         name = params["magic_card_name"].split(" - ")[0].downcase if params["magic_card_name"]
         if name
-          @pagy, @magic_cards = pagy(MagicCard.where('lower(name) LIKE ?', "%#{name}%").order(:name, Arel.sql("CAST(multiverse_ids[1] AS INT) NULLS LAST"), items: 10)
+          @pagy, @magic_cards = pagy(MagicCard.where('lower(name) LIKE ?', "%#{name}%").order(:name, Arel.sql("CAST(multiverse_ids[1] AS INT) NULLS LAST")), items: 10)
         else
           @pagy, @magic_cards = pagy(MagicCard.select(
             :id,
