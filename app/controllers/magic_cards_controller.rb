b25ca785b3cd26ab318cdbb856a69e5a09c231f0
github username <raustin.melchior@gmail.com>
2019-09-30 23:33:50 -0700

    fix search bar

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -38,10 +38,7 @@ class MagicCardsController < ApplicationController
     #   ).where(multiverse_ids: [])
     # end
     # @pagy, @magic_cards = pagy_array(@cards, items: 10)
-    name = params["magic_card_name"].split(" - ") if params["magic_card_name"]
-    puts "pre #{name}"
-    name = name.class == Array ? name.split(" - ")[0] : name
-    puts "Post #{name}"
+    name = params["magic_card_name"].split(" - ")[0] if params["magic_card_name"]
     if name
       @pagy, @magic_cards = pagy(MagicCard.where('lower(name) LIKE ?', "%#{name}%"), items: 10)
     else
