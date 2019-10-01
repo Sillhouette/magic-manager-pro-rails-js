a27a10a73d390d194c64d6cf7749a85ee1ae66fe
github username <raustin.melchior@gmail.com>
2019-10-01 08:21:51 -0700

    prove it to daniel

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -8,41 +8,12 @@ class MagicCardsController < ApplicationController
   end
 
   def index
-    # name = params["magic_card_name"]
-    # name = name.class == Array ? name.split(" - ")[0] : name
-    # if name
-    #   @cards = MagicCard.where('lower(name) LIKE ?', "%#{name}%")
-    # else
-    #   @cards = MagicCard.select(
-    #     :id,
-    #     :image_uris,
-    #     :name,
-    #     :set,
-    #     :set_name,
-    #     :mana_cost,
-    #     :type_line,
-    #     :oracle_text,
-    #     :power,
-    #     :toughness
-    #   ).where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")) + MagicCard.select(
-    #     :id,
-    #     :image_uris,
-    #     :name,
-    #     :set,
-    #     :set_name,
-    #     :mana_cost,
-    #     :type_line,
-    #     :oracle_text,
-    #     :power,
-    #     :toughness
-    #   ).where(multiverse_ids: [])
-    # end
-    # @pagy, @magic_cards = pagy_array(@cards, items: 10)
-    name = params["magic_card_name"].split(" - ")[0] if params["magic_card_name"]
+    name = params["magic_card_name"]
+    name = name.class == Array ? name.split(" - ")[0] : name
     if name
-      @pagy, @magic_cards = pagy(MagicCard.where('lower(name) LIKE ?', "%#{name}%"), items: 10)
+      @cards = MagicCard.where('lower(name) LIKE ?', "%#{name}%")
     else
-      @pagy, @magic_cards = pagy(MagicCard.select(
+      @cards = MagicCard.select(
         :id,
         :image_uris,
         :name,
@@ -53,21 +24,37 @@ class MagicCardsController < ApplicationController
         :oracle_text,
         :power,
         :toughness
-      ).order(Arel.sql("CAST(multiverse_ids[1] AS INT) NULLS LAST")), items: 10)
-      # .merge(MagicCard.select(
-      #   :id,
-      #   :image_uris,
-      #   :name,
-      #   :set,
-      #   :set_name,
-      #   :mana_cost,
-      #   :type_line,
-      #   :oracle_text,
-      #   :power,
-      #   :toughness
-      # ).where(multiverse_ids: []))
+      ).where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")) + MagicCard.select(
+        :id,
+        :image_uris,
+        :name,
+        :set,
+        :set_name,
+        :mana_cost,
+        :type_line,
+        :oracle_text,
+        :power,
+        :toughness
+      ).where(multiverse_ids: [])
     end
-#.where.not(multiverse_ids: [])
+    @pagy, @magic_cards = pagy_array(@cards, items: 10)
+    # name = params["magic_card_name"].split(" - ")[0] if params["magic_card_name"]
+    # if name
+    #   @pagy, @magic_cards = pagy(MagicCard.where('lower(name) LIKE ?', "%#{name}%"), items: 10)
+    # else
+    #   @pagy, @magic_cards = pagy(MagicCard.select(
+    #     :id,
+    #     :image_uris,
+    #     :name,
+    #     :set,
+    #     :set_name,
+    #     :mana_cost,
+    #     :type_line,
+    #     :oracle_text,
+    #     :power,
+    #     :toughness
+    #   ).order(Arel.sql("CAST(multiverse_ids[1] AS INT) NULLS LAST")), items: 10)
+    # end
     respond_to do |format|
       format.html { render :index }
       #format.json { render json: MagicCard.all, include: '*.*'}
