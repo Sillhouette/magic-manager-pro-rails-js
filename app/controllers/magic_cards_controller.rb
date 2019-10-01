cb36129f98710f14cd4f5a441a57482639055cba
github username <raustin.melchior@gmail.com>
2019-09-30 23:08:07 -0700

    change pagination

--- app/controllers/magic_cards_controller.rb
+++ app/controllers/magic_cards_controller.rb
@@ -8,12 +8,42 @@ class MagicCardsController < ApplicationController
   end
 
   def index
+    # name = params["magic_card_name"]
+    # name = name.class == Array ? name.split(" - ")[0] : name
+    # if name
+    #   @cards = MagicCard.where('lower(name) LIKE ?', "%#{name}%")
+    # else
+    #   @cards = MagicCard.select(
+    #     :id,
+    #     :image_uris,
+    #     :name,
+    #     :set,
+    #     :set_name,
+    #     :mana_cost,
+    #     :type_line,
+    #     :oracle_text,
+    #     :power,
+    #     :toughness
+    #   ).where.not(multiverse_ids: []).order(Arel.sql("CAST(multiverse_ids[1] AS INT)")) + MagicCard.select(
+    #     :id,
+    #     :image_uris,
+    #     :name,
+    #     :set,
+    #     :set_name,
+    #     :mana_cost,
+    #     :type_line,
+    #     :oracle_text,
+    #     :power,
+    #     :toughness
+    #   ).where(multiverse_ids: [])
+    # end
+    # @pagy, @magic_cards = pagy_array(@cards, items: 10)
     name = params["magic_card_name"]
     name = name.class == Array ? name.split(" - ")[0] : name
     if name
-      @cards = MagicCard.where('lower(name) LIKE ?', "%#{name}%")
+      @pagy, @magic_cards = pagy(MagicCard.where('lower(name) LIKE ?', "%#{name}%"), items: 10)
     else
-      @cards = MagicCard.select(
+      @pagy, @magic_cards = pagy(MagicCard.select(
         :id,
         :image_uris,
         :name,
@@ -35,12 +65,12 @@ class MagicCardsController < ApplicationController
         :oracle_text,
         :power,
         :toughness
-      ).where(multiverse_ids: [])
+      ).where(multiverse_ids: []), items: 10)
     end
-    @pagy, @magic_cards = pagy_array(@cards, items: 10)
+
     respond_to do |format|
       format.html { render :index }
-      format.json { render json: MagicCard.all, include: '*.*'}
+      #format.json { render json: MagicCard.all, include: '*.*'}
     end
   end
 end
